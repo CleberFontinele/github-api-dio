241ee50456b3926526f7c2c9653b4f9f6bd9635d
CleberFontinele <cleberfontinele2387@gmail.com>
2021-09-27 15:43:00 -0300

    iniciar Readme

---
+++ src/header/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import useGithub from '../hooks/github-hooks';
+import * as S from './styled';
+
+const Header = () => {
+  const { getUser } = useGithub()
+  const [usernameForSearch, setUsernameForSearch] = useState()
+
+  const submitGetUser = () => {
+    if (!usernameForSearch) return;
+    return getUser(usernameForSearch)
+  }
+
+  return (
+    <header>
+      <S.Wrapper>
+        <input
+          type="text"
+          placeholder="Digite o username para pesquisa..."
+          onChange={(e) => setUsernameForSearch(e.target.value)}
+        />
+        <button type="submit" onClick={submitGetUser}>
+          <span>Buscar</span>
+        </button>
+      </S.Wrapper>
+    </header>
+  )
+}
+
+export default Header;
